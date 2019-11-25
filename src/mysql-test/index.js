7073131610a80fa6067248ae4223da327dcc5e8d
zgc <zhangguicheng_1995@163.com>
2019-11-25 10:59:03 +0800

    MySQL connection

--- src/mysql-test/index.js
+++ src/mysql-test/index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-// 创建链接对象
+// 创建连接对象
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -13,7 +13,9 @@ const con = mysql.createConnection({
 con.connect();
 
 // 执行 SQL 语句
-const sql = 'select * from user;';
+// const sql = 'select * from user;';
+// const sql = 'update user set realname="李四2" where username="lisi";';
+const sql = 'insert into blogs (title, content, createtime, author) values ("标题C", "内容C", 1574650349170, "zhangsan");';
 con.query(sql, (err, result) => {
   if(err) {
     console.log(err);
