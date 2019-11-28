6fce674c828cea767e34858002d96b3ec3926a8e
zgc <zhangguicheng_1995@163.com>
2019-11-28 10:17:36 +0800

    file stream

--- src/file-test/test1.js
+++ src/file-test/test1.js
@@ -1,34 +1,76 @@
-const fs = require('fs');
-// 因为每个系统路径的表示方式不同，在这里使用path模块将路径的表示统一化
-const path = require('path');
-
-// __dirname node中自带的一个变量，表示当前文件的目录，可以理解成全局变量
-// 找到当前路径下 data.txt 文件，将路径统一化后再赋给fileName，通过fileName就可以找到data.txt文件。
-const fileName = path.resolve(__dirname, 'data.txt');
-
-// 读取文件内容
-// fs.readFile(fileName, (err, data) => {
+// const fs = require('fs');
+// // 因为每个系统路径的表示方式不同，在这里使用path模块将路径的表示统一化
+// const path = require('path');
+//
+// // __dirname node中自带的一个变量，表示当前文件的目录，可以理解成全局变量
+// // 找到当前路径下 data.txt 文件，将路径统一化后再赋给fileName，通过fileName就可以找到data.txt文件。
+// const fileName = path.resolve(__dirname, 'data.txt');
+//
+// // 读取文件内容
+// // fs.readFile(fileName, (err, data) => {
+// //   if (err) {
+// //     console.error(err);
+// //     return;
+// //   }
+// //   // data 是二进制类型，需要转换为字符串
+// //   console.log(data.toString());
+// // });
+//
+// // 写入文件
+// const content = '这是新写入的内容\n';
+// const opt = {
+//   flag: 'a' // 追加写入，覆盖用 'w'
+// };
+//
+// fs.writeFile(fileName, content, opt, (err) => {
 //   if (err) {
 //     console.error(err);
-//     return;
 //   }
-//   // data 是二进制类型，需要转换为字符串
-//   console.log(data.toString());
+// });
+//
+// // 判断文件是否存在
+// fs.exists(fileName, (exist) => {
+//   console.log('exist', exist); // true 表示存在
 // });
 
-// 写入文件
-const content = '这是新写入的内容\n';
-const opt = {
-  flag: 'a' // 追加写入，覆盖用 'w'
-};
 
-fs.writeFile(fileName, content, opt, (err) => {
-  if (err) {
-    console.error(err);
+// const fs = require('fs');
+// // const path = require('path');
+// //
+// // const fileName1 = path.resolve(__dirname, 'data.txt');
+// // const fileName2 = path.resolve(__dirname, 'data-bak.txt');
+// //
+// // // 读取文件内容
+// // fs.readFile(fileName1, (err, data) => {
+// //   if (err) {
+// //     console.error(err);
+// //     return;
+// //   }
+// //   const dataToString = data.toString();
+// //   // 写入文件
+// //   const opt = {
+// //     flag: 'a' // 追加写入，覆盖用 'w'
+// //   };
+// //   fs.writeFile(fileName2, dataToString, opt, (err) => {
+// //     if (err) {
+// //       console.error(err);
+// //       return;
+// //     }
+// //     console.log('copy done');
+// //   });
+// // });
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const fileName = path.resolve(__dirname, 'data.txt');
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const readStream = fs.createReadStream(fileName);
+    let temp;
+    readStream.pipe(temp); // 读取文件的内容直接返回到res中
+    res = temp.toString();
   }
 });
-
-// 判断文件是否存在
-fs.exists(fileName, (exist) => {
-  console.log('exist', exist); // true 表示存在
-});
+server.listen(8000);
--- src/stream-test/test1.js
+++ src/stream-test/test1.js
@@ -22,4 +22,19 @@
 const fs = require('fs');
 const path = require('path');
 
+const fileName1 = path.resolve(__dirname, 'data.txt');
+const fileName2 = path.resolve(__dirname, 'data-bak.txt');
+
+// 定义两个水桶
+const readStream = fs.createReadStream(fileName1);
+const writeStream = fs.createWriteStream(fileName2);
+
+readStream.pipe(writeStream);
+// 上面的语句等价于
+readStream.on('data', (chunk) => {
+  console.log(chunk); // 当数据文件比较大时，会打印不止一次
+});
+readStream.on('end', ()  => {
+  console.log('copy completed');
+});
 
