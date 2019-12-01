04b13d3ef722c57464827fe6084fc4fa5f84b873
zgc <zhangguicheng_1995@163.com>
2019-12-01 15:08:11 +0800

    PM2 config

--- src/pm2-test/app.js
+++ src/pm2-test/app.js
@@ -1,6 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  // 模拟日志
+  console.log('cur time', Date.now());
+  // 模拟错误
+  console.error('假装出错', Date.now());
+  // 模拟一个错误
+  // 当访问 /err 时模拟出现错误，模拟进程崩溃，看之前能访问的页面是否还能正常访问，并且查看重启次数是否增加
+  if (req.url === '/err') {
+    throw new Error('/err 出错了');
+  }
+
   res.setHeader('content-type', 'application/json');
   res.end(
     JSON.stringify({
